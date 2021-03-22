530fba7295296ae8cab4392cd6b934ce5dec3d08
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2021-03-22 16:42:27 +0100

    Add logging statement and expose metrics and beans in actuator

--- groups/src/main/java/net/novatec/tasc/groups/controller/HelloWorldController.java
+++ groups/src/main/java/net/novatec/tasc/groups/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package net.novatec.tasc.groups.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.novatec.tasc.groups.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 @AllArgsConstructor
+@Slf4j
 public class HelloWorldController {
 
   private final HelloService helloService;
 
   @GetMapping()
   public String hello(){
+    log.info("Everyone says hello to me");
     return helloService.getHelloMessage();
   }
 }
