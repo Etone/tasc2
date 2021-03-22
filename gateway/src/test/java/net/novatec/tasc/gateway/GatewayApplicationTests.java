22365b3c2b25002e8e86cebeb422502b8da7492d
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2021-03-22 16:25:32 +0100

    Add Gateway Service

---
+++ gateway/src/test/java/net/novatec/tasc/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.novatec.tasc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
